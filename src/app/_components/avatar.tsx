139af0922b261d3eb48277dfeafadb469d8d09db
Brad McNew <mcnewbrad@gmail.com>
2024-12-16 12:20:52 -0500

    vercel env vars

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -2,7 +2,6 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
-
 import { signOut } from "next-auth/react";
 
 import {
