b3432981f17c3559446f96289a1965aba6670c33
Brad McNew <mcnewbrad@gmail.com>
2025-08-25 12:58:40 -0400

    feat(posthog): implement posthog analytics provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import '~/styles/globals.css'
 import { GeistSans } from 'geist/font/sans'
 import { type Metadata } from 'next'
 import { Toaster } from 'sonner'
+import { PostHogProvider } from '~/lib/providers/PostHogProvider'
 import { ThemeProvider } from '~/lib/providers/ThemeProvider'
 import { QueryProvider } from '~/lib/react-query/QueryProvider'
 
@@ -35,22 +36,25 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
           enableSystem
           disableTransitionOnChange={false}
         >
-          <QueryProvider>
-            {children}
-            <Toaster
-              theme="system"
-              className="toaster group"
-              toastOptions={{
-                classNames: {
-                  toast:
-                    'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border group-[.toaster]:shadow-lg',
-                  description: 'group-[.toast]:text-muted-foreground',
-                  actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-                  cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
-                },
-              }}
-            />
-          </QueryProvider>
+          <PostHogProvider>
+            <QueryProvider>
+              {children}
+              <Toaster
+                theme="system"
+                className="toaster group"
+                toastOptions={{
+                  classNames: {
+                    toast:
+                      'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border group-[.toaster]:shadow-lg',
+                    description: 'group-[.toast]:text-muted-foreground',
+                    actionButton:
+                      'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+                    cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+                  },
+                }}
+              />
+            </QueryProvider>
+          </PostHogProvider>
         </ThemeProvider>
       </body>
     </html>
