9bfa16057d4f55bf5a77decd4fcbfd41996605de
Brad McNew <mcnewbrad@gmail.com>
2025-08-31 21:56:17 -0400

    feat(settings): add SettingsHeader component and integrate into layout

--- apps/web/src/app/(app)/(mentor)/settings/layout.tsx
+++ apps/web/src/app/(app)/(mentor)/settings/layout.tsx
@@ -1,11 +1,13 @@
 import { AppSidebar } from '~/components/app-sidebar'
 import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar'
+import { SettingsHeader } from './components/SettingsHeader'
 
 export const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        <SettingsHeader />
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
